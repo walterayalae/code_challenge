b955c714a463f8817476d200e86f6750fe4d017f
Walter <walterayalae43@gmail.com>
2017-01-05 17:06:04 -0600

    Setup package.json, server and index.html. Also added eslint. All working

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const browserify = require('browserify-middleware');
+const express = require('express');
+const Path = require('path');
+
+const routes = express.Router();
+
+
+//Providing a browserified fil at a specific path
+routes.get('/app-bundle.js',
+	browserify('./client/app.js', {
+		transform: [[ require('babelify'), {presets:['es2015', 'react']}]]
+	})
+);
+
+const assetFolder = Path.resolve(__dirname, '../client/public');
+routes.use(express.static(assetFolder));
+
+
+var app = express();
+
+app.use('/', routes);
+
+var port =process.env.PORT || 4000;
+app.listen(port);
+console.log('Make some magic on port', port);
